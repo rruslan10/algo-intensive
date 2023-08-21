a179c67ccf7192214d9a0e7d9c0efd14a85ed695
Ruslan <ruxxxa10@gmail.com>
2023-08-21 19:56:22 +0300

    3

---
+++ insertionSort.js
@@ -0,0 +1,19 @@
+const arr = [3, 9, 44, 3, 342, 2, 11, 3, 1, 14]
+
+function solution(arr) {
+	for (let i = 0; i < arr.length; i++) {
+		let j = i
+		let tmp = arr[i]
+
+		while (j > 0 && arr[j - 1] > tmp) {
+			arr[j] = arr[j - 1]
+			j--
+		}
+
+		arr[j] = tmp
+	}
+
+	return arr
+}
+
+console.log(solution(arr))
