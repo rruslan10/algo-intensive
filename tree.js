67e829095ea313e67f9126506fe2e3f36c712472
Ruslan <ruxxxa10@gmail.com>
2023-08-21 20:31:39 +0300

    4 tree

--- tree.js
+++ tree.js
@@ -4,27 +4,70 @@ const tree = {
 		value: 5,
 		left: {
 			value: 2,
+			left: {
+				value: 1,
+			},
+			right: {
+				value: 3,
+			},
 		},
 		right: {
 			value: 7,
+			left: {
+				value: 6,
+			},
+			right: {
+				value: 9,
+			},
 		},
 	},
 	right: {
 		value: 15,
 		left: {
 			value: 12,
+			left: {
+				value: 11,
+			},
+			right: {
+				value: 13,
+			},
 		},
 		right: {
 			value: 18,
+			left: {
+				value: 16,
+			},
+			right: {
+				value: 20,
+			},
 		},
 	},
 }
 
-function solution(val) {
-	console.log(tree.value)
+// Рекурсия
+function solutionRecursion(node) {
+	if (node) {
+		console.log(node.value)
 
-	if (tree.left) solution(tree.left.value)
-	if (tree.right) solution(tree.right.value)
+		if (node.left) solutionRecursion(node.left)
+		if (node.right) solutionRecursion(node.right)
+	}
 }
 
-solution(tree.value)
+// Стек
+function solutionStack(node) {
+	const stack = [node]
+
+	while (stack.length) {
+		const curNode = stack.pop()
+
+		console.log(curNode.value)
+
+		if (curNode.right) stack.push(curNode.right)
+		if (curNode.left) stack.push(curNode.left)
+	}
+}
+
+solutionRecursion(tree)
+console.log()
+solutionStack(tree)
